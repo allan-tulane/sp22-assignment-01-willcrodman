4f5247ab6d8d3306448f54015911d16506739275
Will Rodman <willc.rodman@gmail.com>
2022-02-14 22:36:38 +0000

    final submission

--- main.py
+++ main.py
@@ -6,12 +6,27 @@ See assignment-01.pdf for details.
 
 def foo(x):
     ### TODO
-    pass
+    if x <= 1:
+      return x
+
+    else:
+      ra, rb = foo(x-1), foo(x-2)
+      return ra, rb
 
 def longest_run(mylist, key):
-    ### TODO
-    pass
 
+    count = 0
+    max_count = 0
+
+    for i in mylist:
+      if i == key:
+        count += 1
+        if count > max_count: 
+          max_count = count
+      else:
+        count = 0
+
+    return count
 
 class Result:
     """ done """
@@ -24,14 +39,27 @@ class Result:
     def __repr__(self):
         return('longest_size=%d left_size=%d right_size=%d is_entire_range=%s' %
               (self.longest_size, self.left_size, self.right_size, self.is_entire_range))
-    
+
+
+def in_parallel(f, a, b, key):
+  #theoreticaly thread handler would be implemented here
+  thread_a, thread_b = f(a, key), f(b, key)
+  return thread_a, thread_b
     
 def longest_run_recursive(mylist, key):
-    ### TODO
-    pass
+  
+  if len(mylist) == 1:
+    if key in mylist:
+      return 1
+    else:
+      return 0
+
+  right, left = in_parallel(longest_run_recursive, mylist[:len(mylist)//2], mylist[len(mylist)//2:], key)
+
+  print(Result(right, left, max(right, left), "False"))
+
+  return right + left
 
 ## Feel free to add your own tests here.
 def test_longest_run():
     assert longest_run([2,12,12,8,12,12,12,0,12,1], 12) == 3
-
-
